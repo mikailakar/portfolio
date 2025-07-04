1a2839781a2eede5d34af67550f4d37c02f5b64c
mikailakar <huseyin.akar@bil.omu.edu.tr>
2025-07-04 19:41:39 +0300

    update

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./header/header";
 import { Footer } from "./footer/footer";
-import { Hero } from "./hero/hero";
 import { NgxParticlesModule } from '@tsparticles/angular';
 
 @Component({
@@ -10,7 +9,7 @@ import { NgxParticlesModule } from '@tsparticles/angular';
     standalone: true,
     templateUrl: './app.html',
     styleUrl: './app.css',
-    imports: [RouterOutlet, Header, Footer, Hero, NgxParticlesModule]
+    imports: [RouterOutlet, Header, Footer, NgxParticlesModule]
 })
 export class App {
   title = 'portfolio';
